6e1d6ddda0557292367c80546bc46c34c3b5ae49
zesani <7sin@outlook.co.th>
2018-06-27 16:29:08 +0700

    d3

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">asdasdasdasdasda
-asdasdasdasdasdasddsdasdasdasd
+asdasdasdasdasdasddsdasdasdasdasdasd
     <router-view/>
   </div>
 </template>
