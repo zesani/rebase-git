1b14173e27748ed642a78604525c12143f519155
zesani <7sin@outlook.co.th>
2018-06-27 16:29:18 +0700

    d4

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">asdasdasdasdasda
-asdasdasdasdasdasddsdasdasdasdasdasd
+asdasdasdasdasdasddsdasdasdasdasdasdasdsa
     <router-view/>
   </div>
 </template>
